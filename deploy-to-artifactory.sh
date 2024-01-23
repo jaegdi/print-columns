605d5bc53126f459f206bf813c503b99f7e5c306
Dirk Jäger <dirk.jaeger@schufa.de>
2024-01-23 16:35:47 +0100

    update

--- deploy-to-artifactory.sh
+++ deploy-to-artifactory.sh
@@ -8,10 +8,13 @@ go build
 echo "Build windows binary of pc"
 GOOS=windows GOARCH=amd64 go build
 
+echo "generate ReadMe.md"
+./pc -h > ReadMe.md
+
 echo "Push to artifactory"
-artifactory-upload.sh  pc             scptools-bin-develop   tools/pc
-artifactory-upload.sh  pc.exe         scptools-bin-develop   tools/pc
-artifactory-upload.sh  pc-ReadMe.md   scptools-bin-develop   tools/pc
+artifactory-upload.sh  -lf=pc             -tr=scptools-bin-develop   -tf=tools/pc
+artifactory-upload.sh  -lf=pc.exe         -tr=scptools-bin-develop   -tf=tools/pc
+artifactory-upload.sh  -lf=pc-ReadMe.md   -tr=scptools-bin-develop   -tf=tools/pc
 
 echo "Copy it to share folder PEWI4124://Daten"
 cp pc pc.exe  /gast-drive-d/Daten
