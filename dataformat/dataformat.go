cdf1ab6e64cf3446bf9c0333820bfe7f91d9d0a3
Dirk Jäger <dirk.jaeger@schufa.de>
2024-09-09 11:25:40 +0200

    doc

--- dataformat/dataformat.go
+++ dataformat/dataformat.go
@@ -21,60 +21,85 @@ type T_rawdata []string
 type T_dataline []string
 type T_parsedData []T_dataline
 
+// printJSON prints the parsed data in JSON format.
+// It uses the header line defined in CmdParams.Header and the separator defined in CmdParams.Sep.
+// The function creates a JSON array where each element is an object representing a row of data.
+// Each object contains key-value pairs, where the keys are the column headers and the values are the corresponding data fields.
 func printJSON(d T_parsedData) {
-	//  set seperator rune
+	// Set separator rune
 	sep := []rune(ap.CmdParams.Sep)[0]
-	// split headerline
+	// Split header line using the separator
 	header := LineParse(ap.CmdParams.Header, sep)
+	// Print the opening bracket of the JSON array
 	fmt.Println("[")
 	lines := len(d)
+	// Iterate over each line of data
 	for ln, line := range d {
+		// Print the opening brace of the JSON object
 		fmt.Println("  {")
 		l := len(line)
+		// Iterate over each column value in the line
 		for col, val := range line {
+			// Print the key-value pair for the current column
 			fmt.Print("    \"", header[col], "\": \"", val, "\"")
+			// If not the last column, print a comma
 			if col+1 < l {
 				fmt.Println(",")
 			} else {
 				fmt.Println("")
 			}
 		}
+		// Print the closing brace of the JSON object
 		if ln+1 < lines {
 			fmt.Println("  },")
 		} else {
 			fmt.Println("  }")
 		}
 	}
+	// Print the closing bracket of the JSON array
 	fmt.Println("]")
 }
 
+// printJSONwithTC prints the parsed data in JSON format with a top-level collection.
+// It uses the header line defined in CmdParams.Header and the separator defined in CmdParams.Sep.
+// If the TitleSeparator (Ts) flag is set, it skips the first line of data.
+// The function creates a JSON object where the top-level key is the plural form of the first header field,
+// and the value is an array of objects. Each object represents a row of data, with the first column as a key-value pair
+// and the remaining columns nested under a "data" key.
 func printJSONwithTC(d T_parsedData) {
-	//  set seperator rune
+	// Set separator rune
 	sep := []rune(ap.CmdParams.Sep)[0]
-	// split headerline
+	// Split header line
 	header := d[0]
 	if ap.CmdParams.Header != "" {
 		header = LineParse(ap.CmdParams.Header, sep)
 	}
+	// If TitleSeparator flag is set, skip the first line of data
 	if ap.CmdParams.Ts {
 		d = d[1:]
 	}
+	// Extract the first header key and the remaining headers
 	hkey := header[0]
 	header = header[1:]
 	lines := len(d)
-
+	// Initialize pluralize client
 	pl := pluralize.NewClient()
+	// Print the opening of the JSON object
 	fmt.Println("{")
 	fmt.Print("  \"", pl.Plural(hkey), "\": [")
 	fmt.Println("")
+	// Iterate over each line of data
 	for ln, line := range d {
 		fmt.Println("    {")
+		// Extract the value for the first header key
 		hval := line[0]
 		fmt.Print("      \"", hkey, "\": \"", hval, "\"")
 		fmt.Println(",")
 		fmt.Println("      \"data\": {")
+		// Extract the remaining line values
 		line = line[1:]
 		l := len(line)
+		// Iterate over each column value and print it as a key-value pair
 		for col, val := range line {
 			fmt.Print("        \"", header[col], "\": \"", val, "\"")
 			if col+1 < l {
@@ -84,17 +109,21 @@ func printJSONwithTC(d T_parsedData) {
 			}
 		}
 		fmt.Println("      }")
+		// Print the closing of the current object
 		if ln+1 < lines {
 			fmt.Println("    },")
 		} else {
 			fmt.Println("    }")
 		}
 	}
+	// Print the closing of the JSON array and object
 	fmt.Println("  ]")
 	fmt.Println("}")
 }
 
-// PrintJson prints data as JSON
+// PrintJson prints the parsed data in JSON format.
+// If no header is specified and the TitleSeparator (Ts) flag is not set, it marshals the data directly to JSON.
+// Otherwise, it uses either printJSONwithTC or printJSON based on the CmdParams flags.
 func (d T_parsedData) PrintJson() {
 	if ap.CmdParams.Header == "" && !ap.CmdParams.Ts {
 		b, err := json.MarshalIndent(d, "", "  ")
@@ -111,7 +140,8 @@ func (d T_parsedData) PrintJson() {
 	}
 }
 
-// PrintCsv prints data as CSV
+// PrintCsv prints the parsed data in CSV format.
+// It writes the data to the standard output using the csv.Writer from the encoding/csv package.
 func (d T_parsedData) PrintCsv() {
 	w := csv.NewWriter(os.Stdout)
 	defer w.Flush()
@@ -160,8 +190,10 @@ func (data *T_dataline) generateLine(maxlen T_maxlenghts) {
 	}
 }
 
-// InsertGroupSeperator inserts a trenenr slice when the content of gcol changed the value.
-// Let further values of gcol empty until the next group change
+// InsertGroupSeperator inserts a separator slice when the content of the specified column changes value.
+// It takes a column index, a boolean flag, and two slices (trenner and htrenner) as input.
+// The function inserts a separator slice whenever the value in the specified column changes.
+// It leaves further values of the column empty until the next group change.
 func (data *T_parsedData) InsertGroupSeperator(gcol int, gcolval bool, trenner, htrenner []string) {
 	nd := T_parsedData{}
 	if gcol > 0 && gcol <= len(*data)+1 {
@@ -195,7 +227,9 @@ func (data *T_parsedData) InsertGroupSeperator(gcol int, gcolval bool, trenner, 
 	}
 }
 
-// selectColumns select data columns as defined in CmdParms.Columns
+// selectColumns selects data columns as defined in CmdParams.Columns.
+// It iterates over each row in the parsed data and creates a new row containing only the selected columns.
+// If a column index is out of range, it adds an empty string to the new row.
 func (data *T_parsedData) selectColumns() {
 	if len(ap.CmdParams.Columns) > 0 {
 		for i, row := range *data {
@@ -212,7 +246,9 @@ func (data *T_parsedData) selectColumns() {
 	}
 }
 
-// selectColumns select columns from dataline as defined in CmdParms.Columns
+// selectColumns selects columns from dataline as defined in CmdParams.Columns.
+// It creates a new dataline containing only the selected columns.
+// If a column index is out of range, it adds an empty string to the new dataline.
 func (data *T_dataline) selectColumns() {
 	nrow := T_dataline{}
 	for _, col := range ap.CmdParams.Columns {
@@ -225,7 +261,9 @@ func (data *T_dataline) selectColumns() {
 	*data = nrow
 }
 
-// insertTrenner insert trenner for TitelSeperator, FooterSeperator or PrettyPrint
+// insertTrenner inserts separators for TitleSeparator, FooterSeparator, or PrettyPrint.
+// It takes two slices (trenner and htrenner) as input and inserts them at appropriate positions
+// based on the CmdParams flags.
 func (data *T_parsedData) insertTrenner(trenner, htrenner []string) {
 	if ap.CmdParams.Ts || ap.CmdParams.Fs || ap.CmdParams.Pp {
 		if ap.CmdParams.Pp {
@@ -246,25 +284,37 @@ func (data *T_parsedData) insertTrenner(trenner, htrenner []string) {
 	}
 }
 
-// sort data on k column
+// sort sorts the parsed data based on the specified column index.
+// It takes an integer k as input, which represents the column index to sort by (1-based index).
+// If the NoHeaderLine (Nhl) flag is not set, it treats the first row as a header and excludes it from sorting.
+// The function uses a stable sort to maintain the relative order of rows with equal values in the specified column.
 func (data *T_parsedData) sort(k int) {
+	// Convert 1-based index to 0-based index
 	k -= 1
+	// Initialize variables for header line and data to be sorted
 	l1 := T_dataline{}
 	d := T_parsedData{}
+	// If NoHeaderLine flag is not set, separate the header line from the data
 	if !ap.CmdParams.Nhl {
 		l1 = (*data)[0]
 		d = (*data)[1:]
 	} else {
+		// Otherwise, include all data for sorting
 		d = *data
 	}
+	// Perform a stable sort on the data based on the specified column
 	sort.SliceStable(d, func(i, j int) bool {
 		return d[i][k] < d[j][k]
 	})
+	// Initialize a new parsed data slice to store the sorted data
 	da := T_parsedData{}
+	// If NoHeaderLine flag is not set, add the header line back to the sorted data
 	if !ap.CmdParams.Nhl {
 		da = append(da, l1)
 	}
+	// Append the sorted data to the new parsed data slice
 	da = append(da, d...)
+	// Update the original data with the sorted data
 	*data = da
 }
 
@@ -280,19 +330,28 @@ func (data *T_parsedData) formatDataToMaxWidth(maxlen []int) {
 	}
 }
 
-// printAsciiTab prints the data as ASCII table
+// printAsciiTab prints the parsed data as an ASCII table.
+// It formats each row of the data according to the specified column separator and column width.
+// If the PrettyPrint (Pp) or ColumnSeparator (Cs) flags are set, it adds the column separator between columns.
+// If the MoreBlanks flag is set, it replaces the placeholder character '§' with spaces.
 func (data *T_parsedData) printAsciiTab() {
+	// Create a string of spaces with the length of the column separator width
 	sp := strings.Repeat(" ", ap.CmdParams.ColSepW)
+	// Iterate over each row in the parsed data
 	for _, row := range *data {
 		var line string
+		// If PrettyPrint or ColumnSeparator flags are set, format the row with column separators
 		if ap.CmdParams.Pp || ap.CmdParams.Cs {
 			line = ap.CmdParams.Colsep + sp + strings.Join(row, sp+ap.CmdParams.Colsep+sp) + sp + ap.CmdParams.Colsep
 		} else {
+			// Otherwise, join the columns with spaces
 			line = strings.Join(row, sp)
 		}
+		// If MoreBlanks flag is set, replace '§' with spaces
 		if ap.CmdParams.MoreBlanks {
 			line = strings.Replace(line, "§", " ", -1)
 		}
+		// Print the formatted line
 		fmt.Println(line)
 	}
 }
@@ -308,16 +367,13 @@ func Format(data T_parsedData) {
 	if len(ap.CmdParams.Columns) > 0 {
 		data.selectColumns()
 	}
-
 	// remove header line, first row
 	if ap.CmdParams.Rh {
 		data.delete(0, 1)
 	}
-
 	if ap.CmdParams.SortCol > 0 {
 		data.sort(int(ap.CmdParams.SortCol))
 	}
-
 	// Insert Headerline from CmdParams
 	if ap.CmdParams.Header != "" && !ap.CmdParams.Json {
 		headerline := LineParse(ap.CmdParams.Header, sep)
@@ -327,10 +383,8 @@ func Format(data T_parsedData) {
 		}
 		data.Insert(headerline, 0)
 	}
-
 	// get maxlen for each column
 	maxlen := GetMaxLength(data)
-
 	// insert a row with col numbers
 	if ap.CmdParams.Num {
 		n := []string{}
@@ -345,7 +399,6 @@ func Format(data T_parsedData) {
 		// get maxlen for each column, must calculated again aafter inserting the number line
 		maxlen = GetMaxLength(data)
 	}
-
 	//  define trenner slice
 	trenner := []string{}
 	htrenner := []string{}
@@ -353,32 +406,26 @@ func Format(data T_parsedData) {
 		trenner = append(trenner, strings.Repeat("-", v))
 		htrenner = append(htrenner, strings.Repeat("=", v))
 	}
-
 	// insert trenner for TitelSeperator, FooterSeperator or PrettyPrint
 	if !(ap.CmdParams.Json || ap.CmdParams.Csv) {
 		data.insertTrenner(trenner, htrenner)
 	}
-
 	// print CSV and return
 	if ap.CmdParams.Csv {
 		data.PrintCsv()
 		return
 	}
-
 	// print JSON and return
 	if ap.CmdParams.Json {
 		data.PrintJson()
 		return
 	}
-
 	// insert trenner between GroupChange of gcol
 	data.InsertGroupSeperator(int(ap.CmdParams.Gcol), ap.CmdParams.GcolVal, trenner, htrenner)
-
 	// format all columns for maxlen column width
 	if !ap.CmdParams.Nf {
 		data.formatDataToMaxWidth(maxlen)
 	}
-
 	// print data slices as line with or without column seperator
 	data.printAsciiTab()
 }
