88936cb43b910038af26969172db70786b6cc9f0
Dirk Jäger <dirk.jaeger@schufa.de>
2025-03-19 00:13:48 +0100

    fix

--- Dockerfile
+++ Dockerfile
@@ -14,26 +14,27 @@ RUN yum install -y \
     && yum clean all
 
 # Install Go manually
-ENV GO_VERSION=1.23.7
-RUN wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz && \
+ENV GO_VERSION=1.23.7 \
+    CGO_CFLAGS="-std=c99 -Wno-implicit-function-declaration" \
+    CGO_ENABLED=0 \
+    PATH="/usr/local/go/bin:${PATH}"
+
+RUN wget -q https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz && \
     tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz && \
     rm go${GO_VERSION}.linux-amd64.tar.gz
 
-ENV PATH="/usr/local/go/bin:${PATH}"
 
 # Set the working directory
 WORKDIR /app
 
 # Copy the source code into the container
 COPY . .
-RUN rm go.mod go.sum \
-    && go mod init pc
 
 # Build the pc binary with updated C99 and GCC flags
-ENV CGO_CFLAGS="-std=c99 -Wno-implicit-function-declaration" \
-    CGO_ENABLED=0
-RUN go mod tidy \
- && go build -v -o dist
+RUN rm -f go.mod go.sum \
+ && go mod init pc \
+ && go mod tidy \
+ && go build -v -o ./dist
 
 # Set the entrypoint to the built binary
 CMD ["sleep", "infinity"]
--- build/_build-and-deploy-to-artifactory.sh
+++ build/_build-and-deploy-to-artifactory.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -eo pipefail
 
+BINARY_NAME="pc"
+BINARY_NAME_UBI7="dist/${BINARY_NAME}-ubi7"
+IMAGE="${BINARY_NAME}:ubi7"
+CONTAINER_NAME="${BINARY_NAME}-container"
 
 
 # build the linux binary of pc
@@ -15,11 +19,11 @@ GOOS=windows GOARCH=amd64 go build -v
 if ./pc -c config.yaml -i pw1.1 -f username; then
     echo "Push to artifactory"
 
-    artifactory-upload.sh -lf=pc       -tr=scptools-bin-dev-local   -tf=/tools/pc/pc-linux/
-    artifactory-upload.sh -lf=pc       -tr=scpas-bin-dev-local      -tf=/istag_and_image_management/pc-linux/
+    artifactory-upload.sh -lf=$BINARY_NAME       -tr=scptools-bin-dev-local     -tf="tools/$BINARY_NAME"
+    artifactory-upload.sh -lf=$BINARY_NAME       -tr=scptools-bin-dev-local     -tf="ocp-stable-4.16/clients/$BINARY_NAME"
 
-    artifactory-upload.sh -lf=pc.exe   -tr=scptools-bin-dev-local   -tf=/tools/pcs/pc-windows/
-    artifactory-upload.sh -lf=pc.exe   -tr=scpas-bin-dev-local      -tf=/istag_and_image_management/pc-windows/
+    artifactory-upload.sh -lf=$BINARY_NAME.exe   -tr=scptools-bin-dev-local     -tf="tools/$BINARY_NAME"
+    artifactory-upload.sh -lf=$BINARY_NAME.exe   -tr=scptools-bin-dev-local     -tf="ocp-stable-4.16/clients/$BINARY_NAME"
 
     echo "Copy it to share folder PEWI4124://Daten"
     cp pc pc.exe  /gast-drive-d/Daten/
@@ -28,10 +32,6 @@ fi
 echo
 echo
 echo "#  B U I L D   I M A G E   T O O L   F O R   U B I 7"
-BINARY_NAME="pc"
-BINARY_NAME_UBI7="dist/${BINARY_NAME}-ubi7"
-IMAGE="${BINARY_NAME}:ubi7"
-CONTAINER_NAME="${BINARY_NAME}-container"
 
 # build ubi7 binary in image
 /usr/bin/podman build -t "$IMAGE" -f Dockerfile .
@@ -45,5 +45,6 @@ podman cp "$CONTAINER_NAME":/app/dist/pc "$BINARY_NAME_UBI7"
 scp "$BINARY_NAME_UBI7" cid-scp0-tls-v01-mgmt:
 podman rm "$CONTAINER_NAME"
 
-artifactory-upload.sh -lf="$BINARY_NAME_UBI7" -tr=scptools-bin-dev-local  -tf=ocp-stable-4.16/clients/$BINARY_NAME
+artifactory-upload.sh -lf="$BINARY_NAME_UBI7"   -tr=scptools-bin-dev-local   -tf="tools/$BINARY_NAME"
+artifactory-upload.sh -lf="$BINARY_NAME_UBI7"   -tr=scptools-bin-dev-local   -tf="ocp-stable-4.16/clients/$BINARY_NAME"
 
